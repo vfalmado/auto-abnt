fd6bce4cc6850a5bdeede465f8d1c4f13cba2233
vfalmado <vfalmado@gmail.com>
2020-12-11 23:39:32 +0000

    Cover information   and data warning

--- main.py
+++ main.py
@@ -10,18 +10,34 @@ for section in sections:
   section.bottom_margin = Cm(2)
   section.left_margin = Cm(3)
 
-name = input('Insira o nome que quer no arquivo: ')
+print('Nenhum dos dados colocados aqui são salvos externamente ou enviados para o desenvolvedor, tudo é completamente anônimo e só fica salvo no seu computador.')
+
+file = input('Insira o nome que quer no arquivo: ')
+school = input('Insira o nome do seu colégio: ')
+name = input('Insira seu nome: ')
+city = input('Insira sua cidade: ')
+year = input('Insira o ano de realização do trabalho: ')
 text = input('Insira o texto do seu trabalho: ')
 
-paragraph = document.add_paragraph(text)
+def space():
+  spacing = paragraph.paragraph_format
+  spacing.space_before = Pt(0)
+  spacing.space_after = Pt(0)
+
 paragraph.style = document.styles.add_style('ABNT', WD_STYLE_TYPE.PARAGRAPH)
+paragraph = document.add_paragraph(school)
+space()
+paragraph = document.add_paragraph(name)
+space()
+paragraph = document.add_paragraph(city)
+space()
+paragraph = document.add_paragraph(year)
+space()
+paragraph = document.add_paragraph(text)
+space()
 
 font = paragraph.style.font
 font.name = 'Arial'
 font.size = Pt(12)
 
-spacing = paragraph.paragraph_format
-spacing.space_before = Pt(0)
-spacing.space_after = Pt(0)
-
-document.save(name + ".docx")
+document.save(file + ".docx")
