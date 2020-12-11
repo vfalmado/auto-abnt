65f648f2e66c20693276321a1902b1858a86513c
vfalmado <vfalmado@gmail.com>
2020-12-11 21:14:21 +0000

    Added the possibility to choose the name and content of the file

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import os
-import docx
+from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
+from docx.shared import Pt
 
-doc = docx.Document()
+document = Document()
+
+name = input('Insira o nome que quer no arquivo: ')
+text = input('Insira o texto do seu trabalho: ')
+
+paragraph = document.add_paragraph(texto)
+paragraph.style = document.styles.add_style('ABNT', WD_STYLE_TYPE.PARAGRAPH)
+font = paragraph.style.font
+font.name = 'Arial'
+font.size = Pt(12)
+
+document.save(name + ".docx")
